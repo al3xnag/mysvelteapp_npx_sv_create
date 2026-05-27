9a2034bc311d68e20e0f9cecdc5df204f9466710
Aleksei Nagovitsyn <al3x.nag@gmail.com>
2026-05-27 13:17:22 +0500

    404 2

--- svelte.config.js
+++ svelte.config.js
@@ -8,9 +8,7 @@ const config = {
   },
   kit: {
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter({
-      fallback: '404.html'
-    }),
+    adapter: adapter(),
     paths: {
       base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
     }
